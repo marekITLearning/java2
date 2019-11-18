3ea8b3bb99e14b6c4a5bdf0e36481f0c346fd7d8
ladicky <ladicky@U408PL.itl.lan>
2019-11-18 19:45:36 +0100

    update DbConnection class

--- java2/src/main/java/sk/itlearning/java2/a/db/DbConnection.java
+++ java2/src/main/java/sk/itlearning/java2/a/db/DbConnection.java
@@ -13,19 +13,49 @@ public class DbConnection {
 
 	public static void main(String[] args) {
 
-//		try (Connection connection = DriverManager.getConnection("jdbc:postgresql:zakaznik","postgres", "sa");
-//			Statement stmt = connection.createStatement();
-//			ResultSet rs = stmt.executeQuery("select * from public.zakaznik")) {
-//			while (rs.next()) {
-//				Obcan o = new Obcan("11", rs.getString("name"));
-//				o.setKredit(new BigDecimal(rs.getDouble("kredit")));
-//				System.out.println(o);
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			System.out.println("Nepodarilo sa pripojit na databazu");
-//		}
-		
+		Connection c = null;
+		Statement s = null;
+		ResultSet rs = null;
+		try {
+			c = DriverManager.getConnection("jdbc:postgresql:java", "postgres", "sa");
+			s = c.createStatement();
+			rs = s.executeQuery("select * from zakaznik");
+			while (rs.next()) {
+				Obcan o = new Obcan(rs.getString("zakid"), rs.getString("name"));
+				o.setKredit(rs.getBigDecimal("kredit"));
+				System.out.println(o);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (s != null) {
+					s.close();
+				}
+				if (c != null) {
+					c.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql:zakaznik","postgres", "sa");
+			Statement stmt = connection.createStatement();
+			ResultSet rs2 = stmt.executeQuery("select * from public.zakaznik")) {
+			while (rs2.next()) {
+				Obcan o = new Obcan("11", rs2.getString("name"));
+				o.setKredit(new BigDecimal(rs2.getDouble("kredit")));
+				System.out.println(o);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Nepodarilo sa pripojit na databazu");
+		}
+
 	}
 
 }
