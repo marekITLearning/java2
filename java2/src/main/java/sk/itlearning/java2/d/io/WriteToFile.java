32f0591607fdf9f656999e56c22ba391781180ad
student <student@itl.lan>
2021-09-19 14:03:50 +0200

    update file write

--- java2/src/main/java/sk/itlearning/java2/d/io/WriteToFile.java
+++ java2/src/main/java/sk/itlearning/java2/d/io/WriteToFile.java
@@ -1,13 +1,35 @@
 package sk.itlearning.java2.d.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WriteToFile {
 
 	public static void main(String[] args) {
-		File f = new File("C:\\tmp\\subor.txt");
+		File f = new File("C:/devel/tmp/subor.txt");
+
+		File tmp = new File("C:/devel/tmp/subor1.txt");
+
+		try (InputStream in = new BufferedInputStream(new FileInputStream(f));
+				OutputStream out = new BufferedOutputStream(new FileOutputStream(tmp))) {
+			byte[] buffer = new byte[1024];
+			int lengthRead;
+			while ((lengthRead = in.read(buffer)) > 0) {
+				out.write(buffer, 0, lengthRead);
+				out.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		if (!f.exists()) {
 			try {
@@ -16,14 +38,28 @@ public class WriteToFile {
 				e.printStackTrace();
 				return;
 			}
-        }
+		}
 
-		// using try-with-resource to get automatic close of FileWriter
-		try (FileWriter fw = new FileWriter(f, true)) {
-			fw.append("Prvy riadok" + System.lineSeparator());
-			fw.append("Druhy riadok" + System.lineSeparator());
-			fw.append("Treti riadok" + System.lineSeparator());
-			fw.flush();
+		if (!tmp.exists()) {
+			try {
+				tmp.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+
+		f.setReadOnly();
+
+		try (FileWriter fw = new FileWriter(tmp, true);) {
+			fw.write("Nejaky text\n");
+			f.delete();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Files.move(tmp.toPath(), Paths.get("C:/devel/tmp/subor.txt"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
